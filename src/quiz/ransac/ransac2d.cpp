c766bba68eafb7f402d13aac82a4f49d80d4aad3
kaunghtethtun <kaunghtethtun2013@gmail.com>
2021-01-20 14:45:42 +0630

    v5

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -112,6 +112,66 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 
 }
 
+//for choose road
+std::unordered_set<int> RansacPlane(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+{
+	std::unordered_set<int> inliersResult;
+
+	srand(time(NULL));
+	
+	while(maxIterations--) 
+	{
+		std::unordered_set<int>curr_inliers;
+		while (curr_inliers.size() != 3)
+		{
+			curr_inliers.insert(rand() % cloud->points.size());
+		}
+
+		auto itr = curr_inliers.begin();
+		float x1,y1,z1,x2,y2,z2,x3,y3,z3;
+		x1 = cloud->points[*itr].x;
+		y1 = cloud->points[*itr].y;
+		z1 = cloud->points[*itr].z;
+		itr++;
+		x2 = cloud->points[*itr].x;
+		y2 = cloud->points[*itr].y;
+		z2 = cloud->points[*itr].z;
+		itr++;
+		x3 = cloud->points[*itr].x;
+		y3 = cloud->points[*itr].y;
+		z3 = cloud->points[*itr].z;
+
+		//plane params
+		float a,b,c,d;
+		a = (y2-y1)*(z3-z1) - (z2-z1)*(y3-y1);
+		b = (z2-z1)*(x3-x1) - (x2-x1)*(z3-z1);
+		c = (x2-x1)*(y3-y1) - (y2-y1)*(x3-x1);
+		d = - (a*x1 + b*y1 + c*z1);
+
+		//Measure distance between every point and fitted line
+		for (int i = 0; i < cloud->points.size(); ++i)
+    {
+      if (curr_inliers.count(i) > 0)
+        continue;
+
+      float nx = cloud->points[i].x;
+      float ny = cloud->points[i].y;
+      float nz = cloud->points[i].z;
+      float dist = fabs(a * nx + b * ny + c * nz + d) / sqrt(a * a + b * b + c * c);
+
+      if (dist < distanceTol)
+        curr_inliers.insert(i);
+    }
+
+    if (curr_inliers.size() > inliersResult.size())
+      inliersResult = curr_inliers;
+
+  }
+  
+  return inliersResult;
+
+}
+
 int main ()
 {
 
@@ -119,11 +179,15 @@ int main ()
 	pcl::visualization::PCLVisualizer::Ptr viewer = initScene();
 
 	// Create data
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData3D();
 	
 
 	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 50, 0.5);
+	//std::unordered_set<int> inliers = Ransac(cloud, 50, 0.5);
+
+	// To define road
+	std::unordered_set<int> inliers = RansacPlane(cloud, 50, 0.5);
+
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
